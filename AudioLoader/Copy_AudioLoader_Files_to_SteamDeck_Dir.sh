b6a35781b3318c86ba9552b14c5fb9899d450649
nicballarini <me@alar.io>
2023-11-25 16:10:29 -0600

    cleanup

--- AudioLoader/Copy_AudioLoader_Files_to_SteamDeck_Dir.sh
+++ AudioLoader/Copy_AudioLoader_Files_to_SteamDeck_Dir.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Define the source directories
 MUSIC_DIR="./music/"
 SOUNDS_DIR="./sounds/"
 
-# Define the destination directory
+# AudioLoader destination directory
 DEST_DIR="/home/deck/homebrew/sounds"
 
-# Function to copy directories excluding 'Example'
 copy_directories() {
     for dir in "$1"/*; do
         if [ -d "$dir" ] && [ "$(basename "$dir")" != "Example" ]; then
@@ -16,6 +14,5 @@ copy_directories() {
     done
 }
 
-# Copy directories from both source directories
 copy_directories "$MUSIC_DIR"
 copy_directories "$SOUNDS_DIR"
